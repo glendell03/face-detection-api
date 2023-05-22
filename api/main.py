f8708664adb7d76f1021835bdcbff3363f887de0
Glendell Bringino <dell@Glendells-MacBook-Pro.local>
2023-05-23 01:36:46 +0800

    feat: update vercel build

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-import uvicorn
+# import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
@@ -30,5 +30,5 @@ def health_checker():
     return {"success": True, "date": datetime.now()}
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
